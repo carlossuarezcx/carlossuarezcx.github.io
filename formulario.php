946129c99b2aeb640f2cc3dd48b103b1c05b42c2
Carlos <carlossuarezuwu@gmail.com>
2022-03-12 23:13:21 -0600

    first

---
+++ formulario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 06</title>
+</head>
+<body>  
+    <h3>Practica 06: Numero a letras</h3>
+
+    <form method ="POST" action = "recibir.php">
+         <p>
+              <label>Número a convertir :</label>
+              <input type = "text" name="n"/>
+              
+         </p>  
+    </form>  
+    <?php
+
+    ?>
+</body>
+</html> 
